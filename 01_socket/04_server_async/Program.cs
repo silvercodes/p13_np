053d2fec65172d00c6fbdde00202d5ba71e3ba0d
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2024-03-09 12:47:05 +0300

    add: async socket

---
+++ 01_socket/04_server_async/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const string serverIp = "127.0.0.1";
+const int port = 8080;
+
+
+using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse(serverIp), port);
+
+try
+{
+    socket.Bind(endpoint);
+    socket.Listen(10);
+
+    Console.WriteLine($"Server started at {serverIp}:{port}");
+
+    await RunProcessingAsync();
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+async Task RunProcessingAsync()
+{
+    while (true)
+    {
+        Socket remoteSocket = await socket.AcceptAsync();
+        Console.WriteLine("Connection opened...");
+
+
+        byte[] buffer = new byte[1024];
+        int byteCount = 0;
+        string message = string.Empty;
+        do
+        {
+            byteCount = await remoteSocket.ReceiveAsync(buffer);
+            message += Encoding.UTF8.GetString(buffer, 0, byteCount);
+        } while (remoteSocket.Available > 0);
+
+        Console.WriteLine($"{DateTime.Now.ToShortTimeString()}: {message}");
+
+        Thread.Sleep(2000);
+        string response = "Hello from server! All OK!";
+        
+        await remoteSocket.SendAsync(Encoding.UTF8.GetBytes(response));
+
+        remoteSocket.Shutdown(SocketShutdown.Both);
+        remoteSocket.Close();
+
+        Console.WriteLine("Connection closed...");
+    }
+}
+
+
+
+
+
+
+
+
